d5101d7c2cf4107f2a7df283ff36b6b1c6404938
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-09-30 16:59:18 +0200

    Prim simplified working

--- maze/Main.java
+++ maze/Main.java
@@ -4,16 +4,16 @@ import maze.generationAlgorithms.*;
 
 class tests{
 	public static void main (String[] args) {
-		int rows = 7;
-		int cols = 7;
+		int rows = 15;
+		int cols = 15;
 		// Grid grid = new Grid(rows,cols);
 		// // Cell c = grid.check_and_getCell(0,2);
 		// Sidewinder.buildMaze(grid);
 		// grid.displayGrid();
 		Grid grid = new Grid(rows, cols);
-		Wilson.buildMaze(grid);
-		grid.displayGrid();
-		grid.braid();
+		PrimSimplified.buildMaze(grid);
+		//grid.displayGrid();
+		//grid.braid();
 		Cell start = grid.getCellAt(0, 0);
 		Cell target = grid.getCellAt(rows-1, cols-1);
 
