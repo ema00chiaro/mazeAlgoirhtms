441131da3fd028492e154d18cd6abfa22c835814
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-10-07 18:44:43 +0200

    tests in main

--- maze/Main.java
+++ maze/Main.java
@@ -3,35 +3,33 @@ package maze;
 import maze.cells.Cell;
 import maze.cells.WeightedCell;
 import maze.distances.Distances;
-import maze.distances.WeightedDistances;
 import maze.factories.StandardCellFactory;
 import maze.factories.WeightedCellFactory;
 import maze.generationAlgorithms.*;
 
 class tests{
 	public static void main (String[] args) {
-		int rows = 5;
-		int cols = 5;
+		int rows = 10;
+		int cols = 10;
 
 		//Grid grid = new Grid(rows, cols, new WeightedCellFactory(20));
-		Grid grid = new Grid(rows, cols, new WeightedCellFactory(20));
+		//Grid grid = new Grid(rows, cols, new WeightedCellFactory(20));
+		Grid grid = new Grid(rows, cols, new WeightedCellFactory(10));
 		//grid.setDistances(new WeightedDistances());
 		HuntAndKill.buildMaze(grid);
 		//GrowingTree.buildMaze(grid, list -> Utility.getRandomElement(list));
 		//grid.displayGrid();
-		//grid.braid();
+		grid.braid(1);
 		Cell start = grid.getCellAt(0, 0);
 		Cell target = grid.getCellAt(rows-1, cols-1);
 
-		grid.displayDistances(new Distances().distancesFrom(start).pathTo(target));
-		grid.displayColoredDistances(new Distances().aStarSearch(grid,start, target, (s,t) -> Math.sqrt(Math.pow(s.getRow() - t.getRow(),2) + Math.pow(s.getCol() - t.getCol(), 2))));
-		// grid.displayGrid();
-		// // grid.displayDistances(start);
-		// grid.displayDistances(start);
-		// grid.displayDistanceBetween(start, target);
-		// grid.displayPathBetween(start, target);
-		// //grid.getCellAt(0,1).setWeight(1000);
-		// //grid.displayDistances(start);
-		// //grid.displayPathBetween(start, target);
+		grid.displayDistances(Distances.DijkstraSimplified(start));
+		grid.displayDistances(Distances.DijkstraSimplified(start).pathTo(target));
+		grid.displayDistances(Distances.DijkstraWeights(start));
+		grid.displayDistances(Distances.DijkstraWeights(start).pathTo(target));
+		grid.displayColoredDistances(Distances.deadEndFilling(grid, start, target));
+		grid.displayColoredDistances(Distances.DijkstraWeights(start).pathTo(target));
+		grid.getCellAt(0,1).setWeight(1000);
+		grid.displayColoredDistances(Distances.DijkstraWeights(start).pathTo(target));
 	}
 }
